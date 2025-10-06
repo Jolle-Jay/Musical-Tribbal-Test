e9ae637e568a312ce8d6ab1a062ccfaf592834c7
Jolle-Jay <j.lopezzz@hotmail.com>
2025-10-06 10:31:15 +0200

    created item class.cs

---
+++ itemClass.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel;
+
+namespace App;
+
+class Item
+{
+    public string Name;
+    public string Description;
+    public int Attack;
+
+    public Item(string name, string desc, int attack)
+    {
+        Name = name;
+        Description = desc;
+        Attack = attack;
+    }
+}
